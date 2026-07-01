c7559018ee32ab18afd1d178197d0c9c31f0d586
DESKTOP-I3USAS7\PC <champoyradoo@gmail.com>
2026-07-02 03:38:30 +0800

    chore: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "ph.rentconnect.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10006
-        versionName = "1.0.6"
+        versionCode = 10007
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
